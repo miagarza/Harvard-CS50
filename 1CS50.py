7d528d8e0974a4ccd0b92130587b54fc2bbab349
miagarza <138410247+miagarza@users.noreply.github.com>
2024-07-26 11:16:33 -0500

    Update 1CS50.py

--- 1CS50.py
+++ 1CS50.py
@@ -1,80 +1,80 @@
-#x= int(input("whats x? "))
-#y= int(input("Whats y? "))
+x= int(input("whats x? "))
+y= int(input("Whats y? "))
 
 
-#if x != y:
-#    print("x is not equal to y")
-#else:
-#    print("x is equal to y")
+if x != y:
+    print("x is not equal to y")
+else:
+    print("x is equal to y")
 
 
-#if x<y:
-    #print("x is less than y")
-#elif x>y:
-    #print("x is greater than y")
-#else:
-    #print("x is equal to y")
+if x<y:
+    print("x is less than y")
+elif x>y:
+    print("x is greater than y")
+else:
+    print("x is equal to y")
 
 
 #the elif logic in this scenario allows you to use >= because it bypasses the first questions
-#score= int(input("Score: "))
+score= int(input("Score: "))
 
-#if  score>=90:
- #   print("Grade: A")
-#elif  score >=80:
- #   print("Grade: B")
-#elif  score >= 70:
- #   print("Grade: C")
-#elif score>= 60: 
- #   print("Grade: D")
-#else:
- #   print("Grade: F")
+if  score>=90:
+   print("Grade: A")
+elif  score >=80:
+    print("Grade: B")
+elif  score >= 70:
+    print("Grade: C")
+elif score>= 60: 
+    print("Grade: D")
+else:
+    print("Grade: F")
 
 #parity portion
 
-#x=int(input("What is x? "))
+x=int(input("What is x? "))
 
-#if x %2 ==0:    
- #   print("even")
-#else:
- #   print("Odd")
+if x %2 ==0:    
+    print("even")
+else:
+    print("Odd")
 
-#def main():
- #   x= int(input("What is x? "))
-  #  if is_even(x):
-   #     print("Even")
-    #else:
-     #   print("Odd")
+def main():
+    x= int(input("What is x? "))
+    if is_even(x):
+        print("Even")
+    else:
+        print("Odd")
 
-#def is_even(n):
- #   return n % 2==0 
+def is_even(n):
+    return n % 2==0 
     #part below is other, longer way to write this
     #if n % 2==0:
      #   return True
     #else:
      #   return False
 
-#main()
+main()
 
 
 #HOUSE PORTION
 
-#name= input("whats ur name? ")
+name= input("whats ur name? ")
 
-#if name== "Harry" or name== "Hermione" or name== "Ron":
- #   print("Gryffindor")
-#elif name== "Draco":
-  #  print("Slytherin")
-#else:
-   # print("Who? ")
+if name== "Harry" or name== "Hermione" or name== "Ron":
+    print("Gryffindor")
+elif name== "Draco":
+    print("Slytherin")
+else:
+    print("Who? ")
 
-#name= input("whats ur name? ")
+name= input("whats ur name? ")
 
-#match name:
- #   case "Harry" | "hermione" | "Ron":
-  #      print("Gryffindor")
-   # case "Draco":
-    #    print("Slytherin")
+match name:
+    case "Harry" | "hermione" | "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
 
 
 def main():
