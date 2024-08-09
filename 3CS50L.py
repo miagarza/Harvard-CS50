84416e3437c01ea474289aa1e03839434511f657
miagarza <138410247+miagarza@users.noreply.github.com>
2024-08-08 23:43:07 -0500

    Add files via upload

---
+++ 3CS50L.py
@@ -0,0 +1,23 @@
+"""
+while True:
+    try:
+        x= int(input("Whats x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")
+"""
+def main():
+    x= get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("Whats x? "))
+        except ValueError:
+            pass
+
+main()
