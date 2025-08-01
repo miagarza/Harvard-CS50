38e9a672faf0ac09f9980e68b705c895c1ee096d
miagarza <138410247+miagarza@users.noreply.github.com>
2025-08-01 14:14:35 -0500

    Add files via upload

---
+++ ProblemSetTwo.py
@@ -0,0 +1,168 @@
+#Loops,Videos 3
+
+
+"""
+#meow meow meow
+
+i=1
+
+while i < 3:
+    print("meow")
+    i=i+1
+
+"""
+
+
+"""
+#using _ can be a placeholder for i and it shows its just a 
+#variable that ur not using later 
+
+
+for _ in range(3):
+    print("Meow")
+
+print("meow\n" *3, end="")
+
+"""
+
+"""
+while True:
+    n=int(input("What's n? "))
+    if n>0:
+        break
+   
+
+for _ in range(n):
+    print("meow")
+"""
+
+    
+"""
+def main():
+    number= get_number()
+    meow(number)
+
+
+def get_number():
+    while True:
+        n=int(input("What's n? " ))
+        if n > 0:
+            break
+    return n
+
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+main()
+
+"""
+
+
+"""
+students= ["Harry", "Ron", "Hermione"]
+
+
+#for students in students:
+#    print(students)
+
+for i in range(len(students)):
+    print(i+1 , students[i])
+"""
+
+
+
+"""
+students={"herm": "gryf", 
+          "Harry": "Gryff",
+          "draco": "slyrn"
+}
+
+
+#print(students["herm"])
+#print(students["draco"])
+
+for student in students:
+    print(student, students[student], sep=", ")
+
+
+# in line 84, students[student] is different than the student right 
+#before it because the second variable is getting the value
+# (which is gryff) using the key (harry)
+"""
+
+"""
+#another hp dict
+#lists have square brackets 
+#dict have curly brackets 
+
+
+students=[
+    {"name": "Herm", "house": "gryf", "patr": "otter"},
+    {"name": "harry", "house": "gryf", "patr": "stag"},
+    {"name": "ron", "house": "gryf", "patr": "jack"},
+    {"name": "draco", "house": "slyrn", "patr": None},
+]
+
+for students in students:
+    print(students["name"], students["house"], students["patr"], sep=", ")
+"""
+
+
+"""
+def main():
+    print_column(3)
+
+
+def print_column(height):
+    for _ in range(height):
+        print("#")
+
+main()
+"""
+"""
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("?" * width)
+
+main()
+
+
+"""
+
+"""
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    #for each row in square
+    for i in range(size):
+        #for each brick in row
+        for j in range(size):
+            # print brick 
+            print("#", end="")
+        print()
+
+main()
+"""
+
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    #for each row in square
+    for i in range(size):
+        print_row(size)
+
+def print_row(width):
+    print("#"* width)
+
+
+main()
+
+
